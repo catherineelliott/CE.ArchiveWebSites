2559da18e117f267b7f262e62b06ef808fc0ad84
Matt Williams <matt.williams@leeds.gov.uk>
2019-11-21 14:32:26 +0000

    Added and upgraded admin project to 3.0

---
+++ CE.Admin.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CE.Admin.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
